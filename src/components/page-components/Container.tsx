e3e751b7590bc45528f666aa9ce22500e3a43b74
Jacob Brown <jacob.brown@unity3d.com>
2024-02-12 15:17:26 +0000

    Fix children props

--- src/components/page-components/Container.tsx
+++ src/components/page-components/Container.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { RESUME_DATA } from "@/data/resume-data";
+import { ReactNode } from "react";
 
-function Container({ children }) {
+interface Props {
+  children?: ReactNode
+}
+
+function Container({ children }: Props) {
   return (
     <main className={"container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 " + (RESUME_DATA.isLocal ? 'md:p-2' : 'md:p-16')}>
       {children}
--- src/components/page-components/SectionSpacing.tsx
+++ src/components/page-components/SectionSpacing.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
 import { RESUME_DATA } from "@/data/resume-data";
+import { ReactNode } from "react";
 
-function SectionSpacing({ children }) {
+interface Props {
+  children?: ReactNode
+}
+
+function SectionSpacing({ children }: Props) {
   return (
     <section className={"mx-auto w-full max-w-2xl bg-white print:space-y-4 " + (RESUME_DATA.isLocal ? 'space-y-2 ' : 'space-y-8')}>
       {children}
